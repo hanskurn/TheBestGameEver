70f720079ada9099dde3deead87bce8b5f81772b
Hannah <hannahw283@gmail.com>
2015-03-10 17:34:09 -0700

    fixed test file

--- Test.php
+++ Test.php
@@ -1,9 +1,7 @@
-<html>\
-<p>If PHP is working, you will see "Hello World" below:<hr>\
-<?php\
-   echo "Hello world foo";\
-   phpinfo();  // Print PHP version and config info\
-?>\
-\
-</html>\
-}
+<html>
+If PHP is working, you will see "Hello World" below:
+<?php
+   echo "Hello world foo";
+   phpinfo();  // Print PHP version and config info
+?>
+</html>
